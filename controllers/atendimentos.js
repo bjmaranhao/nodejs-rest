4ed6e61bc3b0fd6acd424cc13605f287ffdbbd6b
Bruno Côrtes <bjmaranhao@gmail.com>
2022-01-08 19:24:42 -0300

    Final Aula 04

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,16 +1,14 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    //Cria uma roda para o /atendimentos responder req do tipo GET e retornar algo
+    //Cria uma rota para o /atendimentos responder req do tipo GET e retornar algo
     app.get('/atendimentos', (req, res)=> res.send('voce está na rota de atendimentos (consign2222).'))
 
     app.post('/atendimentos', (req, res)=> {
         
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('voce está na rota de atendimentos e está realizando um POST.')
-
+        Atendimento.adiciona(atendimento, res)
 
     })
 }
--- models/atendimentos_bkp.js
+++ models/atendimentos_bkp.js
@@ -1,25 +1,27 @@
 const moment = require('moment')
+
 const conexao = require('../infraestrutura/conexao')
+
 
 class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        
-        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
-        const clienteEhValido = atendimento.cliente.length >= 5
+        dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.lenght >= 5
 
         const validacoes = [
             {
-                nome: 'data',
+                nome: data,
                 valido: dataEhValida,
                 mensagem: 'Data deve ser maior ou igual a data atual'
             },
             {
-                nome: 'cliente',
+                nome: cliente,
                 valido: clienteEhValido,
-                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
+
         ]
 
         const erros = validacoes.filter(campo => !campo.valido)
@@ -28,19 +30,22 @@ class Atendimento {
         if(existemErros) {
             res.status(400).json(erros)
         } else {
-            const atendimentoDatado = {...atendimento, dataCriacao, data}
 
+            const atendimentoDatado = {...atendimento, dataCriacao, data} 
             const sql = 'INSERT INTO Atendimentos SET ?'
-    
             conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-                if(erro) {
-                    res.status(400).json(erro)
-                } else {
-                    res.status(201).json(atendimento)
-                }
-            })
+           if (erro) {
+               res.status(400).json(erro)
+           } else {
+               res.status(201).json(resultados)
+           }
+        })
+
         }
-       
+
+
+        
+
     }
 }
 
